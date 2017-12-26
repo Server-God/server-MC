d3639e011a9cb5f99c76ac52f62cd745e5b29016
juice <moses@thevinsons.com>
2017-12-25 16:57:24 -0800

    Initial commit

---
+++ PCRun.bat
@@ -0,0 +1,3 @@
+@echo off
+color 0b
+"C:\Users\mwtb\Documents\Installed\JavaJDK\bin\java.exe" -Xmx1024M -Xms1024M -jar spigot-1.12.2.jar nogui
---
+++ Run.bat
@@ -0,0 +1,3 @@
+@echo off
+color 0b
+java -Xmx1024M -Xms1024M -jar spigot-1.12.2.jar nogui
